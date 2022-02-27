1e4738a3068e9b39b17b709639cd5cd8921bb60b
omokehinde <engr.omokehinde@gmail.com>
2022-02-27 01:22:43 +0100

    Front End Authentication and Authorization
    Implementation of User registration, login and logout fuctionalities.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,35 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { logout, reset } from '../features/auth/authSlice';
 
 function Header() {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const { user } = useSelector((state) => state.auth);
+
+    const onClickLogoutBtn = () => {
+        dispatch(logout());
+        dispatch(reset());
+        navigate('/');
+    };
+
   return (
     <header className='header'>
         <div className='logo'>
             <Link to='/' >Goal Setter</Link>
             <ul>
-                <li>
+                { user ? (
+                    <li>
+                        <button className='btn' onClick={onClickLogoutBtn}>
+                            <FaSignOutAlt /> LogOut
+                        </button>
+                    </li>
+                ) : (
+                    <>
+                    <li>
                     <Link to='/login'>
                         <FaSignInAlt />Login
                     </Link>
@@ -17,6 +39,8 @@ function Header() {
                         <FaUser />Register
                     </Link>
                 </li>
+                    </>
+                ) }
             </ul>
         </div>
     </header>
