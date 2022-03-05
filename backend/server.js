9c118921416c587437c660072a13fd2279228606
omokehinde <engr.omokehinde@gmail.com>
2022-03-05 09:35:25 +0100

    Prepare deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
@@ -14,6 +15,16 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use('/api/goals', require('../backend/routes/goalRoutes'));
 app.use('/api/user', require('../backend/routes/userRoutes'));
+
+// serve frontend
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 
+        '../', 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => res.send('Please set to production.'))
+}
 
 app.use(errorHandler);
 
